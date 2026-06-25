b3124fd2c32794cb9d63cd30970558ba2d75d650
Ayan Nadaf <nadafayan95@gmail.com>
2026-06-25 12:12:10 +0530

    JUnit Basic Testing

---
+++ TDD using JUnit5 and Mockito/1. JUnit Basic Testing/Writing Basic JUnit Tests/Code/Writing Basic JUnit Tests/Code/BasicJUnitTests/src/main/java/app/vercel/ayannadaf/Calculator.java
@@ -0,0 +1,7 @@
+package app.vercel.ayannadaf;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ TDD using JUnit5 and Mockito/1. JUnit Basic Testing/Writing Basic JUnit Tests/Code/Writing Basic JUnit Tests/Code/BasicJUnitTests/src/test/java/app/vercel/ayannadaf/CalculatorTests.java
@@ -0,0 +1,15 @@
+package app.vercel.ayannadaf;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTests {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void greet() {
+        assertEquals(12, calculator.add(5,7));
+    }
+}
