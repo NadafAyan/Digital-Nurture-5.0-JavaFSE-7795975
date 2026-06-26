59453b1d21fb6cfcf8c755f9c39d7b3bcc656b5f
Ayan Nadaf <nadafayan95@gmail.com>
2026-06-26 23:57:14 +0530

    Creating a Spring Boot Application

---
+++ Spring Core and Maven/Creating a Spring Boot Application/Code/LibraryManagement/src/main/java/app/vercel/ayannadaf/LibraryManagement/controller/BookController.java
@@ -0,0 +1,46 @@
+package app.vercel.ayannadaf.LibraryManagement.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    private BookService bookService;
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "API Working!!!";
+    }
+
+    @GetMapping("/books/list")
+    public List<Book> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/books/get/{id}")
+    public Book getBookById(@PathVariable int id){
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping("/books/add")
+    public String addBook( @RequestBody Book book ) {
+        return bookService.addBook(book);
+    }
+
+    @DeleteMapping("/books/delete/{id}")
+    public String deleteBookById(@PathVariable int id){
+        return bookService.deleteBookById(id);
+    }
+
+    @PutMapping("/books/update")
+    public String updateBookById(@RequestBody Book book){
+        return bookService.updateBookById(book);
+    }
+}
---
+++ Spring Core and Maven/Creating a Spring Boot Application/Code/LibraryManagement/src/main/java/app/vercel/ayannadaf/LibraryManagement/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package app.vercel.ayannadaf.LibraryManagement.repository;
+
+import com.example.library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
