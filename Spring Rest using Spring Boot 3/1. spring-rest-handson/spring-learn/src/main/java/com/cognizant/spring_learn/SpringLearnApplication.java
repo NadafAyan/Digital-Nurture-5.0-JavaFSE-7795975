7f7a278039e09047f82800ea41c3d705a97a36fa
Ayan Nadaf <nadafayan95@gmail.com>
2026-07-09 12:43:06 +0530

    Incorporate Logging

--- Spring Rest using Spring Boot 3/1. spring-rest-handson/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Spring Rest using Spring Boot 3/1. spring-rest-handson/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @SpringBootApplication
 public class SpringLearnApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
@@ -24,10 +29,15 @@ public class SpringLearnApplication {
 		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
 
 		try{
+			LOGGER.info("START");
+
 			Date date = format.parse("31/12/2018");
 
 			System.out.println();
 			System.out.println(date);
+//			LOGGER.debug(date);
+
+			LOGGER.info("END");
 		}catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
