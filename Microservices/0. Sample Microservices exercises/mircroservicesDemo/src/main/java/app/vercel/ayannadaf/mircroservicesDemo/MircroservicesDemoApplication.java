fd3455654d88fb143baf60841abf2a4da38b6e9b
Ayan Nadaf <nadafayan95@gmail.com>
2026-07-13 16:28:30 +0530

    Exercise 1: Implementing Centralized Authentication with OAuth 2.1/OIDC

---
+++ Microservices/0. Sample Microservices exercises/mircroservicesDemo/src/main/java/app/vercel/ayannadaf/mircroservicesDemo/MircroservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package app.vercel.ayannadaf.mircroservicesDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MircroservicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MircroservicesDemoApplication.class, args);
+	}
+
+}
---
+++ Microservices/0. Sample Microservices exercises/mircroservicesDemo/src/test/java/app/vercel/ayannadaf/mircroservicesDemo/MircroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vercel.ayannadaf.mircroservicesDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MircroservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
